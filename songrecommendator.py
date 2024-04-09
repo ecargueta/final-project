f0b1707ee6559d802f74f9eb4c7fe25ee2c7da07
ecargueta <eargueta@terpmail.umd.edu>
2024-04-08 22:55:05 -0400

    initial layout

---
+++ songrecommendator.py
@@ -0,0 +1,15 @@
+
+class Recommendator:
+    """
+    """
+    def __init__():
+        pass
+    
+    def user_input():
+        pass
+    
+    def main():
+        pass
+    
+    if __name__== "main":
+        pass
