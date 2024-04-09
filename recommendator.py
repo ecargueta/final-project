505d0f6809615ead1c03b4073d165bdc868d3801
ecargueta <eargueta@terpmail.umd.edu>
2024-04-09 11:10:49 -0400

    docstrings for some of the functions

---
+++ recommendator.py
@@ -0,0 +1,70 @@
+
+class Recommendation:
+  """ A class that holds the song, album, artist recommendation per to the user's input.
+  """
+  def __init__(self):
+      """ Should define the necessary attributes to their corresponding values.
+      """
+      pass
+
+  def user_input():
+      """ The user will be prompt with a message that asks them to first input 
+      genre of music. They will then be asked to input the type of music they want,
+      either album, song, or artist. 
+      
+      Args:
+
+      Return:
+        return user input
+      """
+      pass
+
+  def get_album(album_text):
+      """ If the user inputs that they want a album recommended, this function will
+      select a random album based from the album file.
+      
+      Args:
+        album_text: text file to parse for recommended album
+      """
+      pass
+
+  def get_song(song_text):
+      """ If the user inputs that they want a song recommended, this function will
+      select a random song based from the song file.
+      
+      Args:
+        song_text: text file to parse for recommended song
+      """
+      pass
+
+  def get_artist(artist_text):
+      """ If the user inputs that they want a artist recommended, this function will
+      select a random artist based from the artist file.
+      
+      Args:
+        artist_text: text file to parse for recommended artist
+        
+      """
+      pass
+
+  def read_file():
+      """
+      """
+      pass
+
+  def main():
+      """
+      
+      Return:
+        a tuple containing the wanted type of recommendation by the user and then
+        the recommendation made
+      """
+      pass
+
+  def parse_args():
+      """
+      """
+      pass
+
+  if __name__== "main":
+      pass
