40241fa33d3e721f064add2a993ed2dd86c75320
Cwalker2009 <j8osmund@gmail.com>
2024-04-09 19:51:24 -0400

    Added test functions and pseudocode

--- tester.py
+++ tester.py
@@ -1,2 +1,12 @@
 from recommendator import Recommendation as rec
 from songrecommendator import Recommendation as songrec
+
+
+
+def test1():
+    #use assert to test methods in the reccomendation class recommendator script
+    pass
+
+def test2():
+    #use assert to test methods in the reccomendation class recommendator script
+    pass
