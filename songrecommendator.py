19cefb7515fefeea01098891301fcaaf64f2bb8d
ecargueta <eargueta@terpmail.umd.edu>
2024-04-08 23:08:14 -0400

    potential functions

--- songrecommendator.py
+++ songrecommendator.py
@@ -1,15 +1,46 @@
 
-class Recommendator:
-    """
-    """
-    def __init__():
-        pass
-    
-    def user_input():
-        pass
-    
-    def main():
-        pass
-    
-    if __name__== "main":
-        pass
+class Recommendation:
+  """
+  """
+  def __init__(self):
+      """
+      """
+      pass
+
+  def user_input():
+      """
+      """
+      pass
+
+  def get_album():
+      """
+      """
+      pass
+
+  def get_song():
+      """
+      """
+      pass
+
+  def get_artist():
+      """
+      """
+      pass
+
+  def read_file():
+      """
+      """
+      pass
+
+  def main():
+      """
+      """
+      pass
+
+  def parse_args():
+      """
+      """
+      pass
+
+  if __name__== "main":
+      pass
