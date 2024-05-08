5a06ccd41f50f0796f0c4271390f7ae44b24e78b
ecargueta <eargueta@terpmail.umd.edu>
2024-05-07 23:18:48 -0400

    Changes to main()

--- recommendator.py
+++ recommendator.py
@@ -4,9 +4,7 @@ from argparse import ArgumentParser
 
 class Recommendation:
   """ A class that holds the song, album, artist recommendation per to the user's input."""
-  
-  
-  
+
   def __init__(self):
       """ Should define the necessary attributes to their corresponding values.
       """
@@ -25,8 +23,8 @@ class Recommendation:
         return user input
       """
       
-      self.genre = input("Enter the genre of music: ")
-      self.recommendation_type = input("What type of music do you want? (album, song, or artist): ")
+      self.genre = print(input("Enter the genre of music: "))
+      self.recommendation_type = print(input("What type of music do you want? (album, song, or artist): "))
       return self.genre, self.recommendation_type
 
       
@@ -53,9 +51,6 @@ class Recommendation:
       song_list = df["title"].tolist()
       return rand.choice(song_list)
     
-      
-      
-
   def get_artist(artist_text):
       """ If the user inputs that they want a artist recommended, this function will
       select a random artist based from the artist file.
@@ -65,11 +60,11 @@ class Recommendation:
         
       """
       df = pd.read_csv(artist_text, usecols = ['Artist','artist_count','bpm'])
-      artist_name = df['Artist'].str.split(',', expand=True)[0]#gets single artist and removes the features
+      artist_name = df['Artist'].str.split(',', expand=True)[0]
       artist_list = artist_name.tolist()
       return rand.choice(artist_list)
 
-  def read_file(self):
+  def read_file(self, album_text, song_text, artist_text):
       """once the user inputs all of the recommendations they want, 
       this function will read the file and output the suggessted recommendation(s).
       
@@ -81,13 +76,25 @@ class Recommendation:
       """
       wanted_recommedation = self.user_input()
       if wanted_recommedation == "song":
-        output_recommedation = self.get_song()
+        output_recommedation = self.get_song(song_text)
       elif wanted_recommedation == "artist":
-        output_recommedation = self.get_artist()
+        output_recommedation = self.get_artist(artist_text)
       elif wanted_recommedation == "album":
-        output_recommedation = self.get_album()
+        output_recommedation = self.get_album(album_text)
       return output_recommedation
 
+  def parse_args():
+      """Parse command-line arguments.
+      
+      Args:Args:
+        arglist (list of str): list of arguments from the command line.
+      """
+      parser = ArgumentParser()
+      parser.add_argument("album_csv",type=str, help= "stored data")
+      parser.add_argument("songs_csv", type=str, help= "stored data")
+      parser.add_argument("Artist_csv", type=str, help= "stored data")
+      return parser.parse_args()
+    
   def main():
       """ Will open the text_file or specifc recommendation(s) based on the user's input and desired output
       
@@ -98,21 +105,13 @@ class Recommendation:
         a tuple containing the wanted type of recommendation by the user and then
         the recommendation made
       """
-      pass
-
-  def parse_args():
-      """Parse command-line arguments.
-      
-      Args:Args:
-        arglist (list of str): list of arguments from the command line.
-      """
-      parser =  ArgumentParser()
-      parser.add_argument("album_csv",type=str, help= "stored data")
-      parser.add_argument("songs_csv", type=str, help= "stored data")
-      parser.add_argument("Artist_csv", type=str, help= "stored data")
-      return parser.parse_args()
+      args = Recommendation.parse_args()
+      recommender = Recommendation()
+      recommender = recommender.user_input()
+      recommendation = recommender.read_file(args.album_csv, args.songs_csv, args.Artist_csv)
+      print("Recommended:", recommendation)
 
   if __name__== "main":
     """"Will print the recommendations to the user
     """
-  pass
+    main()  
